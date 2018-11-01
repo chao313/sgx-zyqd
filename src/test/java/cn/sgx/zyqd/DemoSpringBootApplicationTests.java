42f3b9e9d4e35fbf1110635a7f06d52028ae9325
chao <m18888106873@163.com>
2018-11-01 11:00:47 +0800

    添加本地测试环境 - 注意pomjar的变化

--- src/test/java/cn/sgx/zyqd/DemoSpringBootApplicationTests.java
+++ src/test/java/cn/sgx/zyqd/DemoSpringBootApplicationTests.java
@@ -1,49 +1,58 @@
-//package cn.sgx.zyqd;
-//
-//import cn.sgx.zyqd.mybatis.vo.PicAndStationVo;
-//import cn.sgx.zyqd.service.HttpService;
-//import org.junit.Test;
-//import org.junit.runner.RunWith;
-//import org.slf4j.Logger;
-//import org.slf4j.LoggerFactory;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.beans.factory.annotation.Value;
-//import org.springframework.boot.test.context.SpringBootTest;
-//import org.springframework.test.context.junit4.SpringRunner;
-//
-//import java.io.IOException;
-//import java.util.List;
-//
+package cn.sgx.zyqd;
+
+import cn.sgx.zyqd.mybatis.vo.PicAndStationVo;
+import cn.sgx.zyqd.service.HttpService;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
 //@RunWith(SpringRunner.class)
 //@SpringBootTest
-//public class DemoSpringBootApplicationTests {
-//
-//    private Logger logger = LoggerFactory.getLogger(getClass());
-////	http:
-////	limit: 5
-////	host: localhost:65500/chao
-//
-//    @Value(value = "${http.limit}")
-//    private Integer limit;
-//
-//    @Value(value = "${http.limitWeigeht}")
-//    private Integer limitWeigeht;
-//    @Value(value = "${http.host}")
-//    private String host;
-//
-//    @Autowired
-//    HttpService httpService;
-//
-//
-//    @Test
-//    public void testHttpService() {
-//        try {
-//            List<PicAndStationVo> picAndStationVos =
-//                    httpService.getPicAndStationVoLimit(host, limitWeigeht, limit,"dataSource196");
-//            logger.error("[ SUCCESS testHttpService getPicAndStationVoLimit ] vos: {} ",picAndStationVos);
-//        } catch (IOException e) {
-//            logger.error("[ ERROR testHttpService getPicAndStationVoLimit ] msg: {} ",e.getMessage(),e);
-//        }
-//    }
-//
-//}
+public class DemoSpringBootApplicationTests {
+
+    private Logger logger = LoggerFactory.getLogger(getClass());
+//	http:
+//	limit: 5
+//	host: localhost:65500/chao
+
+    @Value(value = "${http.limit}")
+    private Integer limit;
+
+    @Value(value = "${http.limitWeigeht}")
+    private Integer limitWeigeht;
+    @Value(value = "${http.host}")
+    private String host;
+
+    @Autowired
+    HttpService httpService;
+
+
+    @Test
+    public void testHttpService() {
+        try {
+            List<PicAndStationVo> picAndStationVos =
+                    httpService.getPicAndStationVoLimit(host, limitWeigeht, limit,"dataSource196");
+            logger.error("[ SUCCESS testHttpService getPicAndStationVoLimit ] vos: {} ",picAndStationVos);
+        } catch (IOException e) {
+            logger.error("[ ERROR testHttpService getPicAndStationVoLimit ] msg: {} ",e.getMessage(),e);
+        }
+    }
+
+    @Test
+    public void xx(){
+        System.out.println(4.5f == 4.5); // 4.5f会转换成4.5(double类型的）因此true
+        System.out.println(4.4f == 4.4); // false
+    }
+
+}
+
+
